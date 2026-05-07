06a7d35627f2ce320f3a0a02055771aaa735d039
iok11 <ydh960823@hakjisa.co.kr>
2026-05-07 15:40:35 +0900

    수정

--- auth.ts
+++ auth.ts
@@ -41,8 +41,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       if (user) {
         token.id = user.id;
         token.role = (user as any).role;
-        token.username = user.email;
-        token.displayName = user.name;
+        token.username = user.email ?? undefined;
+        token.displayName = user.name ?? undefined;
       }
       return token;
     },
